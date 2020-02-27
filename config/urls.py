fc4d307ac8791103a72e7e0dbebf225a1a0e2d7f
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-27 18:24:30 +0900

    Add home.html

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import page.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', page.views.home),
 ]
 
 urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
