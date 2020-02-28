7793220f618fb0b6eae73eee6c30cf59942b0e56
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-29 01:55:48 +0900

    Add account app

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'page',
+    'account',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import page.urls
+import page.urls, account.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(page.urls)),
+    path('account/', include(account.urls)),
 ]
 
 urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
