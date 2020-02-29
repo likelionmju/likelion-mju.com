557d309ef4ba84ae70a8adcda26c90f01f3ab7bd
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-29 18:30:07 +0900

    Add connect templates to css files

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'page', 'static'),
+	os.path.join(BASE_DIR, 'page/static'),
+    os.path.join(BASE_DIR, 'account/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
