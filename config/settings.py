847781843c1baddfbc7333de63f2da1ba5df48a6
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-02 02:08:11 +0900

    Create custom_user_model

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'account.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
