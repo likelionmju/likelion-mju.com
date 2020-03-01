ce492a41ec702646c7c0e10668a7c914bf854336
CHOI0212 <choiys0311@gmail.com>
2020-03-01 19:42:05 +0900

    username

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name='register'),
     path('logout/',views.logout,name='logout'),
+    path('mail/',views.checkmail,name='checkmail'),
 ]
