2352a847115fbf925047edd71c6a519550c5c950
CHOI0212 <choiys0311@gmail.com>
2020-03-01 13:34:35 +0900

    login1

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name='register'),
+    path('logout/',views.logout,name='logout'),
 ]
