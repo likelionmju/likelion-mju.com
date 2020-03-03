d3221e770b5dab7bd1190c1bd5a23f9c5330ae68
Han-Joon-Hyeok <virq@naver.com>
2020-03-03 12:19:15 +0900

    add 'home.js' file

---
+++ page/static/page/js/home.js
@@ -0,0 +1,13 @@
+/* Demo purposes only */
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
