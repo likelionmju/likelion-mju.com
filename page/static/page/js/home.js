25b60495b9457c43706ebd22b479d916d626efdd
Han-Joon-Hyeok <virq@naver.com>
2020-03-03 20:12:56 +0900

    added moving page 'intro.html' to 'home.html' by meta attribute

--- page/static/page/js/home.js
+++ page/static/page/js/home.js
@@ -1,13 +1,3 @@
-/* Demo purposes only */
-var snippet = [].slice.call(document.querySelectorAll('.hover'));
-if (snippet.length) {
-  snippet.forEach(function (snippet) {
-    snippet.addEventListener('mouseout', function (event) {
-      if (event.target.parentNode.tagName === 'figure') {
-        event.target.parentNode.classList.remove('hover')
-      } else {
-        event.target.parentNode.classList.remove('hover')
-      }
-    });
-  });
-}
+$(window).load(function() {    
+  $('#loading').hide();  
+ });
