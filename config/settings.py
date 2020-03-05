18be01923cdf6427e0c43580591fabc612a85b1c
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-05 12:20:10 +0900

    Change static and media path

--- config/settings.py
+++ config/settings.py
@@ -119,10 +119,10 @@ STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'page/static'),
     os.path.join(BASE_DIR, 'account/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT= os.path.join(BASE_DIR, '.media')
 
 AUTH_USER_MODEL = 'account.User'
 LOGIN_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('account/', include(account.urls)),
 ]
 
-urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
